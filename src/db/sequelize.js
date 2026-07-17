95295168b3b02fb01823734533d93581e88ad255
yeabisra <147205149+yeabisra-b@users.noreply.github.com>
2026-07-17 21:22:13 +0300

    feat: modifed ssl for aiven deplyment

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -3,10 +3,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.Database_url, {
+// Standard regex to pull out your connection details dynamically from your string
+const connectionString = process.env.Database_url;
+const matches = connectionString.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):([^/]+)\/(.+)/);
+
+const [_, username, password, host, port, databaseAndQuery] = matches;
+const database = databaseAndQuery.split('?')[0];
+
+export const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  port: port,
   dialect: 'postgres',
-  logging: false, // Set to console.log to see SQL queries
+  logging: false,
   define: {
     timestamps: false
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Now Node is forced to respect this rule
+    }
   }
 });
